9f5a007af13ffdd4b70f48f914a5e3618cebed07
Diego Robles <roblesd@umich.edu>
2025-12-06 21:45:10 -0500

    day 1 part 2 done

--- day1/day1.go
+++ day1/day1.go
@@ -7,8 +7,12 @@ import (
 	"strconv"
 )
 
+func posMod(a, n int) int {
+	return (a%n + n) % n
+}
+
 func part1(scanner *bufio.Scanner) int {
-	cur_pos := 50
+	curPos := 50
 	total := 0
 
 	for scanner.Scan() {
@@ -21,13 +25,48 @@ func part1(scanner *bufio.Scanner) int {
 
 		switch direction {
 		case 'L':
-			cur_pos = (cur_pos - move) % 100
+			curPos = (curPos - move) % 100
 		case 'R':
-			cur_pos = (cur_pos + move) % 100
+			curPos = (curPos + move) % 100
 		}
 
-		if cur_pos == 0 {
+		if curPos == 0 {
 			total++
+		}
+	}
+	return total
+}
+
+func part2(scanner *bufio.Scanner) int {
+	curPos := 50
+	total := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		direction := line[0]
+		move, err := strconv.Atoi(line[1:])
+		if err != nil {
+			fmt.Println("Couldn't convert string to int")
+			continue
+		}
+
+		rotations := move / 100
+		remainder := posMod(move, 100)
+		total += rotations
+		switch direction {
+		case 'L':
+			if curPos != 0 && curPos-remainder <= 0 {
+				total++
+			}
+			// major key
+			// negative values don't matter if we just care about landing on 0
+			// but it messes up the calculation for moving past 0
+			curPos = posMod((curPos - move), 100)
+		case 'R':
+			if curPos+remainder >= 100 {
+				total++
+			}
+			curPos = (curPos + move) % 100
 		}
 	}
 	return total
@@ -41,6 +80,6 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	pass := part1(scanner)
+	pass := part2(scanner)
 	fmt.Printf("Password is: %d\n", pass)
 }
